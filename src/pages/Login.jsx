f036d8f51ad133f0d60661f2282a891590dfb78a
Mark A. <mark001616@gmail.com>
2024-11-26 22:44:09 -0500

    Add & stylize login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,24 @@
+import "../styles/login.css";
 function Login() {
   return (
     <>
       <div className="content">
-        <form action="/" method="post" className="login-page">
-          <div className="email-section">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-          </div>
-          <div className="password-section">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
-          </div>
-        </form>
+        <div className="login-card">
+          <h1>Log In</h1>
+          <form action="/" method="post" className="login-form">
+            <div className="email-section">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div className="password-section">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" />
+            </div>
+            <button type="submit" className="login-btn">
+              Log In
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
