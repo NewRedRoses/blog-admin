881d55c36701109dbf463bbac30ca44833d6bd07
Mark A. <mark001616@gmail.com>
2024-11-27 23:57:21 -0500

    Add react router as dep

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Login from "./pages/Login.jsx";
+import Manage from "./pages/Manage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/manage",
+    element: <Manage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Login />
+    <RouterProvider router={router} />
   </StrictMode>
 );
