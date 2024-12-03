b440897b1cd4f6c8ac28b3d2392d084f5cc2cadf
Mark A. <mark001616@gmail.com>
2024-12-02 22:34:07 -0500

    Stylize comments and add comments header

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -40,28 +40,31 @@ export default function Post() {
         />
       </div>
 
-      <ul className="comments-container">
-        {comments.map((comment) => {
-          const date = new Date(comment.date_created);
-          const formatter = new Intl.DateTimeFormat("en-US", {
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-          });
-          const formattedDate = formatter.format(date);
-          return (
-            <Comment
-              key={comment.id}
-              author={comment.user.name}
-              text={comment.text}
-              date={formattedDate}
-            />
-          );
-        })}
-      </ul>
+      <div className="comments-container">
+        <h1 className="comments-header">Comments</h1>
+        <ul className="comments-list">
+          {comments.map((comment) => {
+            const date = new Date(comment.date_created);
+            const formatter = new Intl.DateTimeFormat("en-US", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            });
+            const formattedDate = formatter.format(date);
+            return (
+              <Comment
+                key={comment.id}
+                author={comment.user.name}
+                text={comment.text}
+                date={formattedDate}
+              />
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 }
