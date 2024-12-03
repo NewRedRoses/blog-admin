79421fb05af3cbd2f64aba99f7a0638a14814bbf
Mark A. <mark001616@gmail.com>
2024-12-02 22:55:53 -0500

    Add delete comment button and style it a bit

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,20 @@
 import "../styles/comment.css";
+import { Trash2 } from "lucide-react";
 export default function Comment({ author, text, date }) {
+  function handleDelete() {
+    const decision = confirm("Are you sure you want to delete this comment?");
+    if (decision) {
+      //
+    }
+  }
   return (
     <div className="comment-container">
-      <div className="comment-author-container">{author}</div>
+      <div className="comment-top-container">
+        <div className="comment-author-container">{author}</div>
+        <button className="delete-comment-btn" onClick={handleDelete}>
+          <Trash2 />
+        </button>
+      </div>
       <div className="comment-text-container">{text}</div>
       <div className="comment-date-container">{date}</div>
     </div>
